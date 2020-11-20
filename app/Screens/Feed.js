c8c81abdbe8713e24cbcc190c0a93b45b4ea50aa
j0vanni <49281020+j0vanni@users.noreply.github.com>
2020-11-19 21:19:32 -0500

    feed

--- app/Screens/Feed.js
+++ app/Screens/Feed.js
@@ -1,8 +1,23 @@
 import React from "react";
-import { View, Text } from "react-native";
+import SafeAreaView from "react-native-safe-area-view";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-function Feed(props) {
-  return <View></View>;
-}
+import { StreamApp, FlatFeed } from "expo-activity-feed";
+
+const Feed = () => {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }} forceInset={{ top: "always" }}>
+        <StreamApp
+          apiKey="4m4x2gcszs2s"
+          appId="100732"
+          token="jpc7ettmf84zux34ujd4cyp23q72mumnfp6aky3sjzvds6u5u3grajczqyyz5yef"
+        >
+          <FlatFeed />
+        </StreamApp>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
 
 export default Feed;
