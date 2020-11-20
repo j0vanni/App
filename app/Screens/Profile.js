0ff53b577b721796194b33454b2804773da6b77a
NotSalmanNY <57117032+NotSalmanNY@users.noreply.github.com>
2020-11-19 22:33:09 -0500

    profile changes

--- app/Screens/Profile.js
+++ app/Screens/Profile.js
@@ -3,17 +3,23 @@ import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 import {
   StyleSheet,
+  ImageBackground,
   Text,
   View,
   SafeAreaView,
   Image,
   ScrollView,
+  Platform,
 } from "react-native";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 
 export default function App({ navigation }) {
   return (
-    <SafeAreaView style={styles.container}>
+    <ImageBackground
+      source={require("../assets/background.png")}
+      style={styles.container}
+    >
+    <SafeAreaView>
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.titleBar}>
           <Ionicons
@@ -35,22 +41,7 @@ export default function App({ navigation }) {
               resizeMode="center"
             ></Image>
           </View>
-          <View style={styles.dm}>
-            <MaterialIcons
-              name="chat"
-              size={18}
-              color="#DFD8C8"
-            ></MaterialIcons>
-          </View>
-          <View style={styles.active}></View>
-          <View style={styles.add}>
-            <Ionicons
-              name="ios-add"
-              size={48}
-              color="#DFD8C8"
-              style={{ marginTop: 6, marginLeft: 2 }}
-            ></Ionicons>
-          </View>
+          
         </View>
 
         <View style={styles.infoContainer}>
@@ -64,7 +55,7 @@ export default function App({ navigation }) {
 
         <View style={styles.statsContainer}>
           <View style={styles.statsBox}>
-            <Text style={[styles.text, { fontSize: 24 }]}>483</Text>
+            <Text style={[styles.text, { fontSize: 24 }]}>3</Text>
             <Text style={[styles.text, styles.subText]}>Posts</Text>
           </View>
           <View
@@ -77,12 +68,8 @@ export default function App({ navigation }) {
               },
             ]}
           >
-            <Text style={[styles.text, { fontSize: 24 }]}>45,844</Text>
-            <Text style={[styles.text, styles.subText]}>Followers</Text>
-          </View>
-          <View style={styles.statsBox}>
-            <Text style={[styles.text, { fontSize: 24 }]}>302</Text>
-            <Text style={[styles.text, styles.subText]}>Following</Text>
+            <Text style={[styles.text, { fontSize: 24 }]}>0</Text>
+            <Text style={[styles.text, styles.subText]}>Links</Text>
           </View>
         </View>
 
@@ -110,24 +97,6 @@ export default function App({ navigation }) {
               ></Image>
             </View>
           </ScrollView>
-          <View style={styles.mediaCount}>
-            <Text
-              style={[
-                styles.text,
-                { fontSize: 24, color: "#DFD8C8", fontWeight: "300" },
-              ]}
-            >
-              70
-            </Text>
-            <Text
-              style={[
-                styles.text,
-                { fontSize: 12, color: "#DFD8C8", textTransform: "uppercase" },
-              ]}
-            >
-              Media
-            </Text>
-          </View>
         </View>
         <Text style={[styles.subText, styles.recent]}>Recent Activity</Text>
         <View style={{ alignItems: "center" }}>
@@ -137,9 +106,9 @@ export default function App({ navigation }) {
               <Text
                 style={[styles.text, { color: "#41444B", fontWeight: "300" }]}
               >
-                Started following{" "}
-                <Text style={{ fontWeight: "400" }}>Jake Challeahe</Text> and{" "}
-                <Text style={{ fontWeight: "400" }}>Luis Poteer</Text>
+                {" "}
+                <Text style={{ fontWeight: "400" }}></Text> {" "}
+                <Text style={{ fontWeight: "400" }}></Text>
               </Text>
             </View>
           </View>
@@ -150,24 +119,25 @@ export default function App({ navigation }) {
               <Text
                 style={[styles.text, { color: "#41444B", fontWeight: "300" }]}
               >
-                Started following{" "}
-                <Text style={{ fontWeight: "400" }}>Luke Harper</Text>
+                {" "}
+                <Text style={{ fontWeight: "400" }}></Text>
               </Text>
             </View>
           </View>
         </View>
       </ScrollView>
     </SafeAreaView>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FFF",
+    backgroundColor: "pink",
   },
   text: {
-    fontFamily: "HelveticaNeue",
+    fontFamily: Platform.OS === "ios" ? "HelveticaNeue": "",
     color: "#52575D",
   },
   image: {
@@ -192,26 +162,6 @@ const styles = StyleSheet.create({
     height: 200,
     borderRadius: 100,
     overflow: "hidden",
-  },
-  dm: {
-    backgroundColor: "#41444B",
-    position: "absolute",
-    top: 20,
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  active: {
-    backgroundColor: "#34FFB9",
-    position: "absolute",
-    bottom: 28,
-    left: 10,
-    padding: 4,
-    height: 20,
-    width: 20,
-    borderRadius: 10,
   },
   add: {
     backgroundColor: "#41444B",
@@ -245,22 +195,6 @@ const styles = StyleSheet.create({
     overflow: "hidden",
     marginHorizontal: 10,
   },
-  mediaCount: {
-    backgroundColor: "#41444B",
-    position: "absolute",
-    top: "50%",
-    marginTop: -50,
-    marginLeft: 30,
-    width: 100,
-    height: 100,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 12,
-    shadowColor: "rgba(0, 0, 0, 0.38)",
-    shadowOffset: { width: 0, height: 10 },
-    shadowRadius: 20,
-    shadowOpacity: 1,
-  },
   recent: {
     marginLeft: 78,
     marginTop: 32,
@@ -273,7 +207,7 @@ const styles = StyleSheet.create({
     marginBottom: 16,
   },
   activityIndicator: {
-    backgroundColor: "#CABFAB",
+    backgroundColor: "#AB346D",
     padding: 4,
     height: 12,
     width: 12,
